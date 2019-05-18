7653580e02517addafc5fff48abe9d28b5bba937
Long Tran <lngtrn94@gmail.com>
2019-05-18 12:27:47 +0200

    add allowed origins config

--- src/main/java/dev/longi/dragonbrain/config/WebConfig.java
+++ src/main/java/dev/longi/dragonbrain/config/WebConfig.java
@@ -1,6 +1,5 @@
 package dev.longi.dragonbrain.config;
 
-import dev.longi.dragonbrain.util.Environment;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,13 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${env}")
-    private String environment;
+    @Value("${server.allowed-origins}")
+    private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        if (!Environment.PRODUCTION.equals(environment)) {
-            registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-        }
+        registry.addMapping("/**").allowedOrigins(allowedOrigins);
     }
 }
