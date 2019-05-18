14bbb29a82078596b2728ab5667c6fc7d2816bd7
Long Tran <lngtrn94@gmail.com>
2019-05-18 12:19:47 +0200

    remove unused stuff

--- src/main/java/dev/longi/dragonbrain/config/WebConfig.java
+++ src/main/java/dev/longi/dragonbrain/config/WebConfig.java
@@ -1,13 +1,9 @@
 package dev.longi.dragonbrain.config;
 
-import dev.longi.dragonbrain.interceptor.ExecuteTimeInterceptor;
-import dev.longi.dragonbrain.interceptor.UserInterceptor;
 import dev.longi.dragonbrain.util.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,19 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Value("${env}")
     private String environment;
-
-    private final UserInterceptor userInterceptor;
-
-    @Autowired
-    public WebConfig(UserInterceptor userInterceptor) {
-        this.userInterceptor = userInterceptor;
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new ExecuteTimeInterceptor());
-        registry.addInterceptor(userInterceptor);
-    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
